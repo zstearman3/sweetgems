fdf903b5f0d877b7f4d6e1941d48fa8f1f5348c5
Zach Stearman <zstearman3@gmail.com>
2016-07-26 23:25:20 +0000

    Change pictures

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.web_console.whitelisted_ips = '104.129.204.115'
+  config.web_console.whitelisted_ips = '104.129.204.115', '73.206.109.226'
 end
