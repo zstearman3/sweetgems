659463e936a763c0d77c55e674e1f430c96a4c5d
Zach Stearman <zstearman3@gmail.com>
2016-09-13 15:44:53 +0000

    Make admin login before posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  #before_action :logged_in_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :logged_in_admin, only: [:new, :create, :edit, :update, :destroy]
   
   def new
     @post = Post.new
