b83536a68b0511d03f8bf5da849460aada33b6b2
Zach Stearman <zstearman3@gmail.com>
2016-10-07 23:33:35 +0000

    Style order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@ class Order < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :order, presence: true, length: { minimum: 10 }
   
 end
